7897e5b6873ad5fbd93daf6fb99a2ef14702b3bd
Edson Nakamura <edson.nakamura@mercadolivre.com>
2022-07-06 11:37:43 -0300

    refactor db connection

--- adapters/mysql.go
+++ adapters/mysql.go
@@ -8,14 +8,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const (
-	username = "root"
-	password = "my-secret-pw"
-	hostname = "172.17.0.2:3306"
-	dbname   = "test"
-)
+// using hostname and schema from wallet
+func MysqlConnectHS(hostname, dbname string) *sql.DB {
+	return MysqlConnect(USERNAME, PASSWORD, hostname, dbname)
+}
 
-func MysqlConnect() *sql.DB {
+func MysqlConnect(username, password, hostname, dbname string) *sql.DB {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbname))
 	if err != nil {
 		panic(err)
---
+++ adapters/wallet.go
@@ -0,0 +1,8 @@
+package adapters
+
+const (
+	USERNAME = "root"
+	PASSWORD = "my-secret-pw"
+	HOSTNAME = "172.17.0.2:3306"
+	SCHEMA   = "test"
+)
